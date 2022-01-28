ac50cc8e049b9a402a2ce04af241835e513c2861
Fortune <fortunelangat54@gmail.com>
2022-01-28 04:06:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+   function App(){
+      
+
+
+  // POST request using axios with set headers
+  const article = { title: 'amount=1&msisdn=0740408496&account_no=200' };
+  const headers = { 
+    'Apikey': 'Me3s8tLM8vW',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  axios.post('https://tinypesa.com/api/v1/express/initialize/', article, { headers });
+
+
+
+  }; 
 
 export default App;
